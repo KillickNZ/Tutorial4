9b3bda0ab5c8a01c02639cb077fc5b5ae810beba
SEBASTIAN GEORGE CHAPMAN <SEBCHAPMAN@sebastians-MacBook-Pro-2.local>
2020-08-24 16:40:58 +1200

    Added another test and also added another import for junit5

--- src/test/java/tutorial4/TestCalc.java
+++ src/test/java/tutorial4/TestCalc.java
@@ -2,6 +2,7 @@
 import org.junit.jupiter;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 public class TestCalc {
 
@@ -9,6 +10,11 @@ public class TestCalc {
     @Test
     public void testadd(){
         assertEquals(20, Calc.add(10, 10));
+    }
+
+    @Test
+    public void testsubract(){
+        assertEquals(10, Calc.add(20, 10));
     }
 
 
